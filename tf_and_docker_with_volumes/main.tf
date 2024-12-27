7dfb874dd14d5ca989fea7e5ec89200c07bde302
sanjay14073 <103362239+sanjay14073@users.noreply.github.com>
2024-12-27 11:50:13 +0530

    Worked on adding volumes

---
+++ tf_and_docker_with_volumes/main.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "3.0.2"
+    }
+  }
+}
+
+provider "docker" {
+  host = "npipe:////.//pipe//docker_engine"
+}
+
+resource "docker_image" "ubuntu" {
+  name = "ubuntu:latest"
+}
+
+resource "docker_volume" "new_volume" {
+  name="my_volume"
+}
+
+resource "docker_container" "new_container" {
+    image = docker_image.ubuntu.image_id
+    name  = "my_container"
+    entrypoint = ["/bin/bash"]        
+    command    = ["-c", "tail -f /dev/null"] 
+    volumes {
+       volume_name = docker_volume.new_volume.name
+       container_path = "/mnt/my_volume"
+       read_only      = false
+    }
+}
